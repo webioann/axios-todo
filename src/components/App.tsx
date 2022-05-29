665c4d00dd5ed34d3005c1b35dc91bf1576632ff
webioann <ioannn7777@gmail.com>
2022-05-29 11:51:56 +0300

    project init

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './app.scss'
+
+function App() {
+
+    return (
+        <div className='container-fluid'>
+            <div className='container'>
+                <h1>AXIOS</h1>
+            </div>
+        </div>
+    )
+}
+export default App;
